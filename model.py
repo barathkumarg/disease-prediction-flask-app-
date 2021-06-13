4804c92aa850eda67f5965532305856c78166d78
BARATHKUMAR G <62948487+barathkumarking@users.noreply.github.com>
2021-06-13 11:16:59 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pickle
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix,accuracy_score
 from sklearn.model_selection import train_test_split
 
